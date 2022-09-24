e6ec04de33e3f41735b78370a111a0280311cf9c
Marko  Sarcevic <sarcevic.m@outlook.com>
2022-09-24 10:08:16 +0200

    finished navmenu, header, portfolio, footer

---
+++ js.js
@@ -0,0 +1,10 @@
+// arrow Up
+const arrowUp = document.getElementById('arrowUp')
+window.addEventListener('scroll',function(){
+    let scrolled = window.scrollY
+    if (scrolled > 569) {
+        arrowUp.style.display = 'block'
+    } else if(scrolled == 0 ){
+        arrowUp.style.display = 'none'
+    }
+})
